c398b5b51db7db5667dec05a7c74e9ff0e8cde89
Abu Saleh Md. Musa Sazib <musasazib@gmail.com>
2021-08-07 08:05:23 +0600

    Math problem solve

---
+++ math.js
@@ -0,0 +1,33 @@
+// Absolute Number -------------
+const number = -10;
+const output = Math.abs(number);
+console.log(output);
+
+// Ceil Number ---------------
+const number2 = 8.33;
+const output2 = Math.ceil(number2);
+console.log(output2);
+
+// Floor Number -------------
+const number3 = 7.77;
+const output3 = Math.floor(number3);
+console.log(output3);
+
+// Round Number ------------------
+const number4 = 6.49;
+const output4 = Math.round(number4);
+console.log(output4);
+
+// Random Number ------------------
+const output5 = Math.random();
+console.log(output5);
+
+const output6 = Math.random() * 10;
+const rounded = Math.round(output6);
+console.log(rounded);
+
+for (let i = 0; i <= 10; i++) {
+    const output6 = Math.random() * 10;
+    const rounded = Math.round(output6);
+    console.log(rounded);
+}
