01467f3276c55bc8a19c50b747f313ae7cc6d254
Abu Saleh Md. Musa Sazib <musasazib@gmail.com>
2021-08-08 14:27:52 +0600

    Reverse a String

---
+++ reverse.js
@@ -0,0 +1,11 @@
+const greetings = 'Hello, How are you?';
+
+function reverseString(text) {
+    let reverse = '';
+    for (const letter of text) {
+        reverse = letter + reverse;
+    }
+    return reverse;
+}
+const reversed = reverseString(greetings);
+console.log(reversed);
